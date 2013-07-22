a79a57a7ecf50fc0880a9fa1696d2e25865b3345
Kyle Robinson Young <kyle@dontkry.com>
2013-07-22 15:34:41 -0700

    Convert to ndarray

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var THREE = require('three')
+var ndthree = require('ndthree')
 
 module.exports = function(data, mesher, scaleFactor, three) {
   return new Mesh(data, mesher, scaleFactor, three)
@@ -9,44 +10,17 @@ module.exports.Mesh = Mesh
 function Mesh(data, mesher, scaleFactor, three) {
   this.THREE = three || THREE
   this.data = data
-  var geometry = this.geometry = new this.THREE.Geometry()
   this.scale = scaleFactor || new this.THREE.Vector3(10, 10, 10)
-  
-  var result = mesher( data.voxels, data.dims )
-  this.meshed = result
 
-  geometry.vertices.length = 0
-  geometry.faces.length = 0
+  var geometry = this.geometry = new this.THREE.BufferGeometry()
+  var material = this.material = {}
 
-  for (var i = 0; i < result.vertices.length; ++i) {
-    var q = result.vertices[i]
-    geometry.vertices.push(new this.THREE.Vector3(q[0], q[1], q[2]))
-  } 
-  
-  for (var i = 0; i < result.faces.length; ++i) {
-    geometry.faceVertexUvs[0].push(this.faceVertexUv(i))
-    
-    var q = result.faces[i]
-    if (q.length === 5) {
-      var f = new this.THREE.Face4(q[0], q[1], q[2], q[3])
-      f.color = new this.THREE.Color(q[4])
-      geometry.faces.push(f)
-    } else if (q.length == 4) {
-      var f = new this.THREE.Face3(q[0], q[1], q[2])
-      f.color = new this.THREE.Color(q[3])
-      geometry.faces.push(f)
-    }
-  }
-  
-  geometry.computeFaceNormals()
+  // maybe we can simply transpose here to fix orientation?
+  //data.transpose(1, 0, 2)
 
-  geometry.verticesNeedUpdate = true
-  geometry.elementsNeedUpdate = true
-  geometry.normalsNeedUpdate = true
+  var err = ndthree(data, geometry, material)
 
-  geometry.computeBoundingBox()
-  geometry.computeBoundingSphere()
-
+  if (err === false) this.data = false
 }
 
 Mesh.prototype.createWireMesh = function(hexColor) {    
@@ -61,9 +35,19 @@ Mesh.prototype.createWireMesh = function(hexColor) {
   return wireMesh
 }
 
-Mesh.prototype.createSurfaceMesh = function(material) {
-  material = material || new this.THREE.MeshNormalMaterial()
-  var surfaceMesh  = new this.THREE.Mesh( this.geometry, material )
+Mesh.prototype.createSurfaceMesh = function(tiles) {
+  if (this.data === false) {
+    return new this.THREE.Mesh(new this.THREE.Geometry(), new this.THREE.MeshNormalMaterial())
+  }
+
+  var surfaceMesh = ndthree.createMesh({
+    THREE: this.THREE,
+    geometry: this.geometry,
+    // PROBLEM: Why is shader material so slow?
+    material: new this.THREE.ShaderMaterial(this.material),
+    map: tiles,
+  })
+
   surfaceMesh.scale = this.scale
   surfaceMesh.doubleSided = false
   this.surfaceMesh = surfaceMesh
@@ -79,71 +63,3 @@ Mesh.prototype.setPosition = function(x, y, z) {
   if (this.wireMesh) this.wireMesh.position = new this.THREE.Vector3(x, y, z)
   if (this.surfaceMesh) this.surfaceMesh.position = new this.THREE.Vector3(x, y, z)
 }
-
-Mesh.prototype.faceVertexUv = function(i) {
-  var vs = [
-    this.meshed.vertices[i*4+0],
-    this.meshed.vertices[i*4+1],
-    this.meshed.vertices[i*4+2],
-    this.meshed.vertices[i*4+3]
-  ]
-  var spans = {
-    x0: vs[0][0] - vs[1][0],
-    x1: vs[1][0] - vs[2][0],
-    y0: vs[0][1] - vs[1][1],
-    y1: vs[1][1] - vs[2][1],
-    z0: vs[0][2] - vs[1][2],
-    z1: vs[1][2] - vs[2][2]
-  }
-  var size = {
-    x: Math.max(Math.abs(spans.x0), Math.abs(spans.x1)),
-    y: Math.max(Math.abs(spans.y0), Math.abs(spans.y1)),
-    z: Math.max(Math.abs(spans.z0), Math.abs(spans.z1))
-  }
-  if (size.x === 0) {
-    if (spans.y0 > spans.y1) {
-      var width = size.y
-      var height = size.z
-    }
-    else {
-      var width = size.z
-      var height = size.y
-    }
-  }
-  if (size.y === 0) {
-    if (spans.x0 > spans.x1) {
-      var width = size.x
-      var height = size.z
-    }
-    else {
-      var width = size.z
-      var height = size.x
-    }
-  }
-  if (size.z === 0) {
-    if (spans.x0 > spans.x1) {
-      var width = size.x
-      var height = size.y
-    }
-    else {
-      var width = size.y
-      var height = size.x
-    }
-  }
-  if ((size.z === 0 && spans.x0 < spans.x1) || (size.x === 0 && spans.y0 > spans.y1)) {
-    return [
-      new this.THREE.Vector2(height, 0),
-      new this.THREE.Vector2(0, 0),
-      new this.THREE.Vector2(0, width),
-      new this.THREE.Vector2(height, width)
-    ]
-  } else {
-    return [
-      new this.THREE.Vector2(0, 0),
-      new this.THREE.Vector2(0, height),
-      new this.THREE.Vector2(width, height),
-      new this.THREE.Vector2(width, 0)
-    ]
-  }
-}
-;
